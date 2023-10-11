2ae6f9a2a551f55deb2f766ba84016cd1703e09b
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-11 03:42:50 -0300

    feat: Creating MessageResponse Class

---
+++ DesafioTecnicoAlunoTurma/Models/MessageResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace DesafioTecnicoAlunoTurma.Models
+{
+    public class MessageResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public MessageResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public MessageResponse(bool success)
+        {
+            Success = success;
+        }
+    }
+}
