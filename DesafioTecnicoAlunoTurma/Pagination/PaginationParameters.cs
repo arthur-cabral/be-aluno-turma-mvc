0a563047d4b26666ad5a68cf38f076519d900786
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-12 03:53:28 -0300

    feat: Adding pagination in project

---
+++ DesafioTecnicoAlunoTurma/Pagination/PaginationParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace DesafioTecnicoAlunoTurma.Pagination
+{
+    public class PaginationParameters
+    {
+        const int maxPageSize = 100;
+        public int PageNumber { get; set; } = 0;
+        private int _pageSize { get; set; } = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
