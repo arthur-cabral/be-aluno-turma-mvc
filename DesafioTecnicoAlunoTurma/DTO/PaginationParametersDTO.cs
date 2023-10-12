be9d42d2a8d959bd477ba29ca52b21b95bd30e4d
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-12 15:14:38 -0300

    chore: Adding PaginationParametersDTO

---
+++ DesafioTecnicoAlunoTurma/DTO/PaginationParametersDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DesafioTecnicoAlunoTurma.DTO
+{
+    public class PaginationParametersDTO
+    {
+        public int PageNumber { get; set; } = 0;
+        public int PageSize { get; set; } = 10;
+    }
+}
