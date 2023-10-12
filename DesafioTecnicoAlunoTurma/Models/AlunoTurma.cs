afa2514659030e35118a9318011d247101f4d948
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-11 21:31:40 -0300

    feat: Creating AlunoTurma model

---
+++ DesafioTecnicoAlunoTurma/Models/AlunoTurma.cs
@@ -0,0 +1,12 @@
+﻿namespace DesafioTecnicoAlunoTurma.Models
+{
+    public class AlunoTurma
+    {
+        public int Id { get; set; }
+        public Aluno Aluno { get; set; } = new Aluno();
+        public int AlunoId { get; set; }
+        public Turma Turma { get; set; } = new Turma();
+        public int TurmaId { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
